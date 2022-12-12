0d994463ded16b693f2c80bb2a368bda570e6f25
Aidan Steele <aidan.steele@glassechidna.com.au>
2022-12-13 08:20:15 +1100

    fix crasher in images with no env vars

--- components/envVars.tsx
+++ components/envVars.tsx
@@ -2,7 +2,9 @@ import {Code, Table} from "@mantine/core";
 import styles from "../styles/Home.module.css";
 
 export function EnvVars({config}: { config: any }) {
-  const env: { key: string, val: string }[] = config.config.Env.map((kv: string) => {
+  const rawEnv = config.config.Env || []; // Env can be null, see mcr.microsoft.com/windows/server:ltsc2022@sha256:c1ab506591d9cf636a7b37202568db62b7d1bbfcdb6181fad093660f5a6b8b15
+
+  const env: { key: string, val: string }[] = rawEnv.map((kv: string) => {
     const [key, ...vals] = kv.split("=");
     return {key, val: vals.join("=")};
   }).sort((a: any, b: any) => a.key.localeCompare(b.key));
